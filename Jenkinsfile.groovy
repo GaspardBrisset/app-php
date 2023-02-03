3606a8118d7249433e4d11617560d71e3c09ce9b
GaspardBrisset <60252798+GaspardBrisset@users.noreply.github.com>
2023-02-03 15:08:27 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,15 +1,13 @@
 node {
 
    def registryProjet='brisset/'
-  def IMAGE="${registryProjet}app:3.5"*/
+  
 
     stage('Clone') {
           checkout scm
     }
 
-    def img = stage('Build') {
-          docker.build("$IMAGE",  '.')
-    }
+ 
 /*
     stage('Run') {
           img.withRun("--name run-$BUILD_ID -p 8000:80") { c ->
@@ -18,11 +16,11 @@ node {
     }*/
 
     stage('Push') {
-   /* docker.withRegistry('https://index.docker.io/v1/' , 'hub_docker_id') {
+    docker.withRegistry('https://index.docker.io/v1/' , 'hub_docker_id') {
            /*  // img.push 'latest'
             //  img.push()
        //echo  "push"
-          }*/
+         */ }
        echo "test"
     }
 

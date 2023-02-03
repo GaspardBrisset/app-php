c40d9c8ff4a14c10ab7ce5e229001873158f8828
GaspardBrisset <60252798+GaspardBrisset@users.noreply.github.com>
2023-02-03 15:47:04 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -8,7 +8,6 @@ node {
     }
 
     stage('Push') {
-    docker.withRegistry('https://index.docker.io/v1/' , 'hub_docker_id') {   
          sh 'apk update'
          sh  'apk add docker-compose'
          sh 'docker-compose up -d'

0c93da63a9e2ca6ee7f6ec4456ea44616a170f3b
GaspardBrisset <60252798+GaspardBrisset@users.noreply.github.com>
2023-02-03 11:28:16 +0100

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -19,8 +19,9 @@ node {
 
     stage('Push') {
     docker.withRegistry('https://index.docker.io/v1/' , 'hub_docker_id') {
-              img.push 'latest'
-              img.push()
+           /*   img.push 'latest'
+              img.push()*/
+       echo  "push"
           }
        echo "test"
     }
